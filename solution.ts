bd88d025994f6c05b0905dbab67e4dd764a164b9
Kooreis <kooreis.official@gmail.com>
2023-10-21 18:03:53 -0500

    This line of the script calculates the middle index of the merged array. If the length of the array is odd, it rounds down to the nearest whole number.

--- solution.ts
+++ solution.ts
@@ -1,3 +1,1 @@
-function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-    const merged: number[] = [...nums1, ...nums2].sort((a, b) => a - b);
-}
+const mid = Math.floor(merged.length / 2);
