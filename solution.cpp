dd569fc74923a8e2d5db745a398149e055e34e41
Kooreis <kooreis.official@gmail.com>
2023-10-21 18:03:53 -0500

    Define a function to find the median of two sorted arrays

--- solution.cpp
+++ solution.cpp
@@ -1,3 +1,10 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
+    nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+    std::sort(nums1.begin(), nums1.end());
+    int n = nums1.size();
+    if (n % 2 == 0) {
+        return (nums1[n / 2 - 1] + nums1[n / 2]) / 2.0;
+    } else {
+        return nums1[n / 2];
+    }
+}
